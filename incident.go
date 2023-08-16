8521e5d4022a560184ac1ad51d18fbf2eaa0cf2f
apernouille <alexis.desrochettes@orange.fr>
2023-08-16 19:35:48 +0200

    fix "json: invalid use of string struct tag" error

--- incident.go
+++ incident.go
@@ -81,7 +81,7 @@ func (incident *Incident) GetComponentStatus(cfg *CachetMonitor) (int, error) {
 	}
 
 	var data struct {
-		Status int `json:"status,string"`
+		Status int `json:"status"`
 	}
 	if err := json.Unmarshal(body.Data, &data); err != nil {
 		return 0, fmt.Errorf("Cannot parse component body: %v. Err = %v", string(body.Data), err)
